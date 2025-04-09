7a90101915157f8f2be7b0cdf08b7a1fe3f74658
AchrafSL <thenewAchraf2020@gmail.com>
2025-04-09 13:06:15 +0100

    Ai_xml_done

--- app/src/main/java/com/example/edimydar/AiFragment.java
+++ app/src/main/java/com/example/edimydar/AiFragment.java
@@ -3,20 +3,34 @@ package com.example.edimydar;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class AiFragment extends Fragment {
-
+    RecyclerView recyclerView;
+    TextView welcomeTxt,msgTxt;
+    ImageView sendBnt;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_ai, container, false);
-        return v;
+        View view = inflater.inflate(R.layout.fragment_ai, container, false);
+
+        // references :
+        recyclerView = view.findViewById(R.id.recyclerView);
+        welcomeTxt = view.findViewById(R.id.welcomeTxtAI);
+        msgTxt = view.findViewById(R.id.msgTxt);
+        sendBnt = view.findViewById(R.id.sendBtn);
+
+
+
+        return view;
     }
 }
